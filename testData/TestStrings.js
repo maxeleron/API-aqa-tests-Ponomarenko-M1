efb6115a4a40b3264221ad87b42d5ec647fb2365
Ivan Chepurkin <maxeleron@gmail.com>
2021-12-16 19:32:39 +0200

    Added last scenarios

--- testData/TestStrings.js
+++
@@ -1,7 +0,0 @@
-// class TestStrings {
-//     constructor() {
-//       this.catalogueButtonText = 'Каталог';
-//       this.catalogueButtonText1 = 'Каталог';
-//     }
-//   }
-//   module.exports = { TestStrings };
--- tests/6.Files.spec.js
+++ tests/6.Files.spec.js
@@ -1,19 +1,33 @@
 const { test, expect } = require('@playwright/test');
 
+const REPO = 'API-test-repo';
+const USER = 'maxeleron';
+
 // 12. Test - Check the file exists
 test('[API] Check `playwright.config.js` file exists', async ({ request }) => {
-  const response = await request.get('/maxeleron/PlaywrightFirstSteps/blob/main/playwright.config.js');
+  const response = await request.get(`/${USER}/PlaywrightFirstSteps/blob/main/playwright.config.js`);
 
   expect(response).toBeTruthy();
 });
 
 // 13. Test - Check file metadata
 test('[API] Check `maxeleron` user info is correct', async ({ request }) => {
-  const response = await request.get('repos/maxeleron/PlaywrightFirstSteps/blob/main/playwright.config.js');
+  const response = await request.get(`repos/${USER}/PlaywrightFirstSteps/blob/main/playwright.config.js`);
 
-  // Assertion for user login
   expect(response["_headers"]["_headersArray"][0]["name"]).toEqual("Server");
   expect(response["_headers"]["_headersArray"][0]["value"]).toEqual("GitHub.com");
   expect(response["_headers"]["_headersArray"][2]["name"]).toEqual("Content-Type");
   expect(response["_headers"]["_headersArray"][2]["value"]).toEqual("application/json; charset=utf-8");
   });
+
+// 14. Test - Creating File within repository
+test('[API] Should create a file', async ({ request }) => {
+  const fileQuery = await request.put(`/repos/${USER}/${REPO}/file.txt`);
+  expect(fileQuery.ok()).toBeTruthy();
+});
+
+// 15. Test - Check file deletion from repository
+test('[API] Should remove a file', async ({ request }) => {
+  const fileQuery = await request.delete(`/repos/${USER}/${REPO}/file.txt`);
+  expect(fileQuery.ok()).toBeTruthy();
+});
