45a39b32ac0ac02409d3fdb6d7414b2f73978bcb
Ivan Chepurkin <maxeleron@gmail.com>
2021-12-16 10:18:39 +0200

    Commented test data

--- testData/TestStrings.js
+++ testData/TestStrings.js
@@ -1,7 +1,7 @@
-class TestStrings {
-    constructor() {
-      this.catalogueButtonText = 'Каталог';
-      this.catalogueButtonText1 = 'Каталог';
-    }
-  }
-  module.exports = { TestStrings };
+// class TestStrings {
+//     constructor() {
+//       this.catalogueButtonText = 'Каталог';
+//       this.catalogueButtonText1 = 'Каталог';
+//     }
+//   }
+//   module.exports = { TestStrings };
