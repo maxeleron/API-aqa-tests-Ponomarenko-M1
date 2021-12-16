b65c7a322d2851c7c10c0f60bdb05c2f694ba37f
Ivan Chepurkin <maxeleron@gmail.com>
2021-12-16 10:24:30 +0200

    updated playwright.config

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,17 @@
+// playwright.config.js
+// @ts-check
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-  workers: 1,
-  retries: 2,
   use: {
-    headless: false,
-    viewport: { width: 1980, height: 1080 },
-    launchOptions: {
-      slowMo: 1000,
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://api.github.com',
+    extraHTTPHeaders: {
+      // We set this header per GitHub guidelines.
+      'Accept': 'application/vnd.github.v3+json',
+      // Add authorization token to all requests.
+      // Assuming personal access token available in the environment.
+      'Authorization': `token INSERT_YOUR_GITHUB_TOKEN_HERE`,
     },
-    },
-  projects: [
-    {
-      name: 'Desktop Chromium',
-      use: {
-        browserName: 'chromium',
-      },
-    },
-  ],
+  }
 };
-
 module.exports = config;
